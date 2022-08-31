b984c769c01bd2f4d75a576b49beb7961e234e05
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-01 01:23:36 +0200

    Initial Commit

---
+++ Slot_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot_Game
+//
+//  Created by M. A. Alim Mukul on 01.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slot_Game/Slot_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_GameApp.swift
+//  Slot_Game
+//
+//  Created by M. A. Alim Mukul on 01.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
